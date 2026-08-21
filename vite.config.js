09749987b13d4e8762ede47bea44f7e72517e0eb
dreamyworm <dreamyworm@gmail.com>
2026-08-21 11:09:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/prismatic-burst-demo/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
